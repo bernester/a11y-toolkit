af7773ae579c37b282003523f2697bf1e9e81b70
Berni Ruoff <berni.ruoff@thoughtworks.com>
2024-04-23 14:56:35 +0200

    removed unused code from ffoter

--- src/routes/footer.svelte
+++ src/routes/footer.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-	import * as config from '$lib/config';
-</script>
-
 <!-- Footer -->
 <footer>
 	<!-- <p>The A11y toolkit is created </p> -->
@@ -10,9 +6,5 @@
 <style>
 	footer {
 		padding-block: var(--size-7);
-	}
-
-	p {
-		color: var(--text-2);
 	}
 </style>
