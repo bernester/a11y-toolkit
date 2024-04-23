cfb862f6d90cb212c5115fad1173e08dbc5b8015
Berni Ruoff <berni.ruoff@thoughtworks.com>
2024-04-23 12:48:26 +0200

    updated level filter to keep focus

--- src/components/LevelFilter.svelte
+++ src/components/LevelFilter.svelte
@@ -8,7 +8,7 @@
 	export let selectedLevel: Level;
 
 	function updateLevel(newLevel: Level) {
-		goto(`?level=${newLevel}`, { replaceState: true, invalidateAll: true });
+		goto(`?level=${newLevel}`, { replaceState: true, keepFocus: true });
 	}
 
 	function handleLevelChange(event: RadioOnChangeEvent) {
@@ -17,31 +17,41 @@
 	}
 </script>
 
-<form>
-	<fieldset class="space-3">
-		<legend class="page-subheader space-1">Select a conformance level</legend>
-		<div class="radiobutton-group">
-			<div class="radiobutton">
-				<RadioButton value="A" isChecked={selectedLevel === 'A'} onChange={handleLevelChange}
-					><strong>Basic</strong><br />
-					<small>Level A</small>
-				</RadioButton>
-			</div>
-			<div class="radiobutton">
-				<RadioButton value="AA" isChecked={selectedLevel === 'AA'} onChange={handleLevelChange}
-					><strong>Recommended</strong><br />
-					<small>Level AA and lower</small>
-				</RadioButton>
-			</div>
-			<div class="radiobutton">
-				<RadioButton value="AAA" isChecked={selectedLevel === 'AAA'} onChange={handleLevelChange}
-					><strong>Advanced</strong><br />
-					<small>Level AAA and lower</small>
-				</RadioButton>
-			</div>
+<fieldset class="space-3">
+	<legend class="page-subheader space-1">Select a conformance level</legend>
+	<div class="radiobutton-group">
+		<div class="radiobutton">
+			<RadioButton
+				value="A"
+				isChecked={selectedLevel === 'A'}
+				onChange={handleLevelChange}
+				name="level"
+				><strong>Basic</strong><br />
+				<small>Level A</small>
+			</RadioButton>
 		</div>
-	</fieldset>
-</form>
+		<div class="radiobutton">
+			<RadioButton
+				value="AA"
+				isChecked={selectedLevel === 'AA'}
+				onChange={handleLevelChange}
+				name="level"
+				><strong>Recommended</strong><br />
+				<small>Level AA and lower</small>
+			</RadioButton>
+		</div>
+		<div class="radiobutton">
+			<RadioButton
+				value="AAA"
+				isChecked={selectedLevel === 'AAA'}
+				onChange={handleLevelChange}
+				name="level"
+				><strong>Advanced</strong><br />
+				<small>Level AAA and lower</small>
+			</RadioButton>
+		</div>
+	</div>
+</fieldset>
 
 <style lang="scss">
 	.radiobutton-group {
