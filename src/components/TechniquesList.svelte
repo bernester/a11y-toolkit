53fbb7ff6d364ae01c4822e2ce2c611f5b1897c1
Berni Ruoff <berni.ruoff@thoughtworks.com>
2024-04-26 10:10:06 +0200

    added a message of no techniques available

--- src/components/TechniquesList.svelte
+++ src/components/TechniquesList.svelte
@@ -1,22 +1,31 @@
 <script lang="ts">
 	import TechniqueCard from '$components/TechniqueCard.svelte';
 	import type { TechniqueMeta } from '$types/types';
+	import { fly } from 'svelte/transition';
 
 	export let techniques: TechniqueMeta[] = [];
 </script>
 
 <ul class="techniques">
-	{#each techniques as technique}
+	{#if techniques.length > 0}
+		{#each techniques as technique}
+			<li>
+				<TechniqueCard
+					href={`/${technique.slug}`}
+					title={technique.title}
+					description={technique.description || ''}
+					level={technique.level}
+					components={technique.components}
+				/>
+			</li>
+		{/each}
+	{:else}
 		<li>
-			<TechniqueCard
-				href={`/${technique.slug}`}
-				title={technique.title}
-				description={technique.description || ''}
-				level={technique.level}
-				components={technique.components}
-			/>
+			<div class="no-result" in:fly={{ y: 10 }}>
+				<h2>No techniques available for your selection.</h2>
+			</div>
 		</li>
-	{/each}
+	{/if}
 </ul>
 
 <style lang="scss">
@@ -30,6 +39,20 @@
 		li {
 			padding-left: 0;
 			max-inline-size: inherit;
+		}
+	}
+
+	.no-result {
+		padding: var(--size-4);
+		border: 2px solid var(--border);
+		text-align: center;
+		display: flex;
+		justify-content: center;
+		border-radius: var(--size-2);
+
+		h2 {
+			font-size: var(--font-size-fluid-1);
+			margin: 0;
 		}
 	}
 </style>
