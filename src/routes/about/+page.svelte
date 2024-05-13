4671ab60e75a4e67acab39cde4017dfa6a09a688
Berni Ruoff <berni.ruoff@thoughtworks.com>
2024-05-13 11:05:57 +0200

    edited about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,34 +1,25 @@
+<script lang="ts">
+	import Link from '$components/Link.svelte';
+</script>
+
 <svelte:head>
 	<title>What is the A11y toolkit?</title>
 </svelte:head>
 
 <h1>What's the A11y toolkit?</h1>
 <p>
-	Since 2019 the organisation WebAIM (Web Accessibility In Mind) cunducts an yearly accessibility
-	evaluation of the home pages for the top 1,000,000 web sites.
+	Since 2019 the organisation WebAIM (Web Accessibility In Mind) conducts an yearly accessibility
+	evaluation of the homepages for the top 1,000,000 web sites.
 </p>
 <p>
-	Their <a href="https://webaim.org/projects/million/" target="_blank">WebAim Million study</a> has shown
-	that 95.9% of home pages in 2024 had accessibility failures. Most of those failures are really simple
-	to fix and to prevent.
+	Their <Link href="https://webaim.org/projects/million/" target="_blank">WebAim Million study</Link
+	> has shown that 95.9% of home pages in 2024 had accessibility failures. Most of those failures are
+	really simple to fix and to prevent.
 </p>
 <h2>We want to fix this</h2>
 <p>
 	The A11y toolkit shows you techniques and best practices how to avoid the most common
 	accessibility failures.
-</p>
-<h2>You want to contribute?</h2>
-<p>
-	You also have discovered or created a technique that should be in the A11y toolkit? <a
-		href="mailto:a11y-checklist@thoughtworks.com">Share your technique with us</a
-	>
-</p>
-<h2>From Thoughtworks with ❤️ love</h2>
-<p>The A11y toolkit was created and developed by an exeptional group of Thoughtworkers:</p>
-<p>
-	Andreas Klumpp, Andreas Kutschmann, Berni Ruoff, Daniel Borchert Avalone, Engie Tawfik, Eva
-	Rupprecht, Ezgi Erol, Guilherme Prestes, Magda Lenart, Mica Beneke, Michael Luis Calvert, Mona
-	Fenzl, Pandora Cousins, Sumedha Verma, Tanuj Mathur
 </p>
 
 <style lang="scss">
