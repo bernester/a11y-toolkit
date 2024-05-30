787989f9b0a723c8b17f7fc6ecf389702917b9c3
Bernhard Ruoff <bernesterland@gmail.com>
2024-05-30 16:46:44 +0200

    updated the about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -12,18 +12,26 @@
 
 <h1>What's the A11y toolkit?</h1>
 <p>
-	Since 2019 the organisation WebAIM (Web Accessibility In Mind) conducts an yearly accessibility
-	evaluation of the homepages for the top 1,000,000 web sites.
+	The A11y toolkit is an open source collection for techniques to avoid the most common
+	accessibility failures. The A11y toolkit started as a community project at <Link
+		href="https://webaim.org/projects/million/"
+		target="_blank">Thoughtworks</Link
+	> and later was launched as an open source project to help other to build better products for more
+	people.
 </p>
 <p>
-	Their <Link href="https://webaim.org/projects/million/" target="_blank">WebAim Million study</Link
-	> has shown that 95.9% of home pages in 2024 had accessibility failures. Most of those failures are
-	really simple to fix and to prevent.
+	The inspiration for the A11y toolkit came from the <Link
+		href="https://webaim.org/projects/million/"
+		target="_blank">WebAim Million report</Link
+	>. Since 2019 WebAIM (Web Accessibility In Mind) conducts this yearly accessibility evaluation of
+	the homepages for the top 1,000,000 web sites.
 </p>
-<h2>We want to fix this</h2>
 <p>
-	The A11y toolkit shows you techniques and best practices how to avoid the most common
-	accessibility failures.
+	The report consistently shows that most homepages have accessibility failures (95.9% in 2024) and
+	that most failures belong to five categories: Low contrast text (81%), missing alternative text
+	for images (54.5%), missing form input labels (48.6%), empty links (44.6%), empty buttons (28.2%)
+	and missing document language (17.1%). Which is great news, because all of them are really easy to
+	fix.
 </p>
 
 <style lang="scss">
