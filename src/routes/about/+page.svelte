d052e8322899d782e83d88c06a2c276146ddbb4a
Bernhard Ruoff <bernesterland@gmail.com>
2024-06-10 08:35:05 +0200

    fixed typo on about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -12,7 +12,7 @@
 
 <h1>What's the A11y toolkit?</h1>
 <p>
-	The A11y toolkit is an open source collection for techniques to avoid the most common
+	The A11y toolkit is an open source collection of techniques to avoid the most common
 	accessibility failures. The A11y toolkit started as a community project at <Link
 		href="https://www.thoughtworks.com/"
 		target="_blank">Thoughtworks</Link
