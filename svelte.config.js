dadeaff07ba640b2328df0e243c460a7aa12d32f
Bernhard Ruoff <bernesterland@gmail.com>
2024-10-03 09:39:43 +0200

    switched to vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import { transformerNotationHighlight } from '@shikijs/transformers';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { mdsvex, escapeSvelte } from 'mdsvex';
 import { getHighlighter } from 'shiki';
